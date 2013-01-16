5ab74c952e6f80386e8850cb79f7056f439d7451
Andrey Breslav <andrey.breslav@jetbrains.com>
2013-01-16 19:08:31 +0400

    Package class name computation changed

--- compiler/tests/org/jetbrains/jet/asJava/KotlinLightClassTest.java
+++ compiler/tests/org/jetbrains/jet/asJava/KotlinLightClassTest.java
@@ -131,7 +131,7 @@ public abstract class KotlinLightClassTest extends KotlinAsJavaTestBase {
         }
 
         public void testPackage() throws Exception {
-            checkModifiers("test." + PackageClassUtils.getPackageClassName(new FqName("test")), PUBLIC, FINAL);
+            checkModifiers("test.TestPackage", PUBLIC, FINAL);
         }
     }
 
