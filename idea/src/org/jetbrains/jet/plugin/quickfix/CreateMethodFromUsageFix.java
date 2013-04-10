0b0c36ea361e35353bedf03b0b2eca00653d12f1
Jack Zhou <yz282@cornell.edu>
2013-04-09 22:23:43 -0400

    Changed setupFunctionBody to static.

--- idea/src/org/jetbrains/jet/plugin/quickfix/CreateMethodFromUsageFix.java
+++ idea/src/org/jetbrains/jet/plugin/quickfix/CreateMethodFromUsageFix.java
@@ -497,7 +497,7 @@ public class CreateMethodFromUsageFix extends CreateFromUsageFixBase {
                 ApplicationManager.getApplication().runWriteAction(new Runnable() {
                     @Override
                     public void run() {
-                        setupFunctionBody(project, func, isUnit, ownerTypeDescriptor);
+                        setupFunctionBody(project, func, methodName, isUnit, ownerTypeDescriptor);
                     }
                 });
 
@@ -506,7 +506,11 @@ public class CreateMethodFromUsageFix extends CreateFromUsageFixBase {
         });
     }
 
-    private void setupFunctionBody(@NotNull Project project, @NotNull JetNamedFunction func, boolean isUnit,
+    private static void setupFunctionBody(
+            @NotNull Project project,
+            @NotNull JetNamedFunction func,
+            @NotNull String funcName,
+            boolean isUnit,
             @NotNull ClassifierDescriptor ownerTypeDescriptor
     ) {
         FileTemplate fileTemplate = FileTemplateManager.getInstance().getCodeTemplate(TEMPLATE_FROM_USAGE_METHOD_BODY);
@@ -520,7 +524,7 @@ public class CreateMethodFromUsageFix extends CreateFromUsageFixBase {
         }
         properties.setProperty(FileTemplate.ATTRIBUTE_CLASS_NAME, DescriptorUtils.getFQName(ownerTypeDescriptor).getFqName());
         properties.setProperty(FileTemplate.ATTRIBUTE_SIMPLE_CLASS_NAME, ownerTypeDescriptor.getName().getName());
-        properties.setProperty(FileTemplate.ATTRIBUTE_METHOD_NAME, methodName);
+        properties.setProperty(FileTemplate.ATTRIBUTE_METHOD_NAME, funcName);
 
         @NonNls String bodyText;
         try {
